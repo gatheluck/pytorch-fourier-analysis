bbf0571c81ac203295002d161da2295f9e165458
gatheluck <gatheluck+tech@gmail.com>
2020-10-09 06:59:01 +0900

    add shared.py

---
+++ pytorch_fourier_analysis/shared.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import torch
+import torchvision
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from pytorch_fourier_analysis import models
+
+
+def get_model(
+    name: str, num_classes: int, inplace: bool = True, **kwargs
+) -> torch.nn.Module:
+
+    # select model
+    if name == "resnet50":
+        model = torchvision.models.resnet50(pretrained=False, num_classes=num_classes)
+    elif name == "wideresnet40":
+        model = models.wideresnet40(
+            num_classes=num_classes, widening_factor=kwargs["widening_factor"]
+        )
+    else:
+        raise NotImplementedError
+
+    # relpace ReLU.inplace
+    for m in model.modules():
+        if isinstance(m, torch.nn.ReLU):
+            m.inplace = inplace
+
+    return model
