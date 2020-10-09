f0387cb2f2f910c1296c2c8538a1ec05509f33d5
gatheluck <gatheluck+tech@gmail.com>
2020-10-09 21:00:19 +0900

    add test code for base.py

---
+++ tests/augmentations/test_base.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import torch
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from pytorch_fourier_analysis import augmentations
+
+
+class TestMixAugmentationBase:
+    def test_calc_mixed_loss(self):
+        output = torch.randn(16, 10)
+        t = torch.randint(low=0, high=10, size=(16,), dtype=torch.long)
+        rand_index = torch.randperm(t.size(0))
+        criterion = torch.nn.CrossEntropyLoss()
+
+        ce_loss_a = criterion(output, t)
+        ce_loss_b = criterion(output, t[rand_index])
+        assert (
+            augmentations.MixAugmentationBase()
+            ._calc_mixed_loss(output, t, rand_index, 1.0, criterion)
+            .equal(ce_loss_a)
+        )
+        assert (
+            augmentations.MixAugmentationBase()
+            ._calc_mixed_loss(output, t, rand_index, 0.0, criterion)
+            .equal(ce_loss_b)
+        )
+        assert (
+            augmentations.MixAugmentationBase()
+            ._calc_mixed_loss(output, t, rand_index, 0.5, criterion)
+            .equal((0.5 * ce_loss_a) + (0.5 * ce_loss_b))
+        )
