5d6456b5237c391b183ebd637928ae76437c1e5f
gatheluck <gatheluck+tech@gmail.com>
2020-10-09 21:03:20 +0900

    apply black to test code

--- tests/augmentations/test_cutmix.py
+++ tests/augmentations/test_cutmix.py
@@ -8,10 +8,12 @@ from pytorch_fourier_analysis import augmentations
 
 if __name__ == "__main__":
     import torchvision
-    import functools
+
     model = torchvision.models.resnet50(num_classes=10, pretrained=False)
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-    dataset = torchvision.datasets.CIFAR10(root="../../data", train=False, transform=transform, download=True)
+    dataset = torchvision.datasets.CIFAR10(
+        root="../../data", train=False, transform=transform, download=True
+    )
     loader = torch.utils.data.DataLoader(dataset, shuffle=False, batch_size=32)
     criterion = torch.nn.CrossEntropyLoss()
 
