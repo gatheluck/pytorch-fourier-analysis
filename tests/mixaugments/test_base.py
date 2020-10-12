b479f018ad5e9b9af6d269e803b924ddc5fe83f6
gatheluck <gatheluck+tech@gmail.com>
2020-10-12 23:09:10 +0900

    add test for sample mask

--- tests/mixaugments/test_base.py
+++ tests/mixaugments/test_base.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+import pytest
 import torch
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
@@ -29,4 +31,23 @@ class TestMixAugmentationBase:
             mixaugments.MixAugmentationBase()
             ._calc_mixed_loss(output, t, rand_index, 0.5, criterion)
             .equal((0.5 * ce_loss_a) + (0.5 * ce_loss_b))
+        )
+
+    # test data for test_sample_mask
+    params = {
+        "full size mask": (
+            torch.Size([3, 32, 32]),
+            -1,
+            torch.ones(3, 32, 32, dtype=torch.bool),
+        )
+    }
+
+    @pytest.mark.parametrize(
+        "input_size, mask_size, expect", list(params.values()), ids=list(params.keys())
+    )
+    def test_sample_mask(self, input_size, mask_size, expect):
+        assert (
+            mixaugments.MixAugmentationBase()
+            ._sample_mask(input_size, mask_size)
+            .equal(expect)
         )
