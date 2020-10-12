c4585805be8d618bfb06106d64380f8a54240a73
gatheluck <gatheluck+tech@gmail.com>
2020-10-12 15:02:24 +0900

    change dataset path

--- tests/mixaugments/test_cutmix.py
+++ tests/mixaugments/test_cutmix.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     model = torchvision.models.resnet50(num_classes=10, pretrained=False)
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
     dataset = torchvision.datasets.CIFAR10(
-        root="../../data", train=False, transform=transform, download=True
+        root="../../data/cifar10", train=False, transform=transform, download=True
     )
     loader = torch.utils.data.DataLoader(dataset, shuffle=False, batch_size=32)
     criterion = torch.nn.CrossEntropyLoss()
@@ -25,4 +25,4 @@ if __name__ == "__main__":
         print(retdict["output"].shape)
         print(retdict["x"].shape)
         torchvision.utils.save_image(retdict["x"], "../../logs/cutmix_x.png")
-        raise NotImplementedError
+        break
