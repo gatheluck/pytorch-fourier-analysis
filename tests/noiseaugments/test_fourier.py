9929e5484a412c27583f9a2653e4344e86281650
gatheluck <gatheluck+tech@gmail.com>
2020-10-16 00:01:45 +0900

    add test code for fourier basis augmentation

---
+++ tests/noiseaugments/test_fourier.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+
+import pytorch_fourier_analysis.shared
+import pytorch_fourier_analysis.noiseaugments
+
+
+class TestFourier:
+    def test_generate_sample(self, cifar10):
+        fourier_augment = pytorch_fourier_analysis.noiseaugments.Fourier(
+            prob=1.0, max_index=15, max_eps=4.0
+        )
+
+        dataset = cifar10
+        dataset.transform.transforms.append(fourier_augment)  # dataset.transform is torchvision.transforms.Compose object
+        loader = torch.utils.data.DataLoader(dataset, 32, shuffle=False, num_workers=8)
+
+        for x, _ in loader:
+            torchvision.utils.save_image(x, "logs/fourier.png")
+            break
