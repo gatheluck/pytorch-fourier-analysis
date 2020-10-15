0e6366c11da992864c1b4610d2cd3aa373703448
gatheluck <gatheluck+tech@gmail.com>
2020-10-16 00:50:14 +0900

    add test code for patch fourier

--- tests/noiseaugments/test_fourier.py
+++ tests/noiseaugments/test_fourier.py
@@ -12,9 +12,32 @@ class TestFourier:
         )
 
         dataset = cifar10
-        dataset.transform.transforms.append(fourier_augment)  # dataset.transform is torchvision.transforms.Compose object
+        dataset.transform.transforms.append(
+            fourier_augment
+        )  # dataset.transform is torchvision.transforms.Compose object
         loader = torch.utils.data.DataLoader(dataset, 32, shuffle=False, num_workers=8)
 
         for x, _ in loader:
             torchvision.utils.save_image(x, "logs/fourier.png")
+            break
+
+
+class TestPatchFourier:
+    def test_generate_sample(self, cifar10):
+        patch_fourier_augment = pytorch_fourier_analysis.noiseaugments.PatchFourier(
+            prob=1.0,
+            patch_size=25,
+            randomize_patch_size=True,
+            max_index=15,
+            max_eps=4.0,
+        )
+
+        dataset = cifar10
+        dataset.transform.transforms.append(
+            patch_fourier_augment
+        )  # dataset.transform is torchvision.transforms.Compose object
+        loader = torch.utils.data.DataLoader(dataset, 32, shuffle=False, num_workers=8)
+
+        for x, _ in loader:
+            torchvision.utils.save_image(x, "logs/patch_fourier.png")
             break
