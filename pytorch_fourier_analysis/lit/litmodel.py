3f2df65ba4cbfcc93353cb74d81f39fe6b74e3df
gatheluck <gatheluck+tech@gmail.com>
2020-10-24 02:39:55 +0900

    add pgd-at

--- pytorch_fourier_analysis/lit/litmodel.py
+++ pytorch_fourier_analysis/lit/litmodel.py
@@ -3,10 +3,11 @@ import sys
 import torch
 import torchvision
 import pytorch_lightning as pl
-from typing import Union
+from typing import Optional
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from pytorch_fourier_analysis.mixaugments.base import MixAugmentationBase
+from pytorch_fourier_analysis.attacks.attack import AttackWrapper
 from pytorch_fourier_analysis.lit.logger import get_epoch_end_log
 from pytorch_fourier_analysis.shared import calc_error
 
@@ -16,7 +17,8 @@ class ClassificationModel(pl.LightningModule):
         self,
         model: torch.nn.Module,
         criterion: torch.nn.modules.loss._Loss,
-        mixaugment: Union[None, MixAugmentationBase],
+        mixaugment: Optional[MixAugmentationBase],
+        attack_class: Optional[AttackWrapper],
         optimizer_class: torch.optim.Optimizer,
         scheculer_class: torch.optim.lr_scheduler._LRScheduler,
     ):
@@ -24,6 +26,8 @@ class ClassificationModel(pl.LightningModule):
         self.model = model
         self.criterion = criterion
         self.mixaugment = mixaugment
+        self.attack_class = attack_class
+        self.attack = None
         self.optimizer_class = optimizer_class
         self.scheduler_class = scheculer_class
 
@@ -39,9 +43,16 @@ class ClassificationModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, t = batch
 
+        if self.attack_class:  # apply adversarial training
+            # NOTE: pytorch lightning automatically decide device. so we have to instantiation attack here
+            if self.attack is None:
+                self.attack = self.attack_class(device=self.device)
+            x = self.attack(self.model, x, t)
+            self.model.zero_grad()  # NOTE without zero_grad leads nan gradient
+
         if self.mixaugment:  # apply mix augmentation
             loss, retdict = self.mixaugment(self.model, self.criterion, x, t)
-        else:  # vanilla train
+        else:
             output = self.model(x)
             loss = self.criterion(output, t)
             retdict = dict(x=x.detach(), output=output.detach(), loss=loss.detach())
