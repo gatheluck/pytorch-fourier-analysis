4e84c7255cf13311c508ae0796bc2ac3056612b8
gatheluck <gatheluck+tech@gmail.com>
2020-10-23 22:09:48 +0900

    visualize corruption type

--- pytorch_fourier_analysis/eval/variance.py
+++ pytorch_fourier_analysis/eval/variance.py
@@ -14,13 +14,21 @@ def main(target_dir: str, drop_gauss: bool, **kwargs):
         df = pd.read_csv(csvpath, index_col="Unnamed: 0")
         df = df.iloc[0:-3, :]
         df = df[df["corruption"] != "gaussian_noise"] if drop_gauss else df
-        print("mean:{mean:0.2f}, std:{std:0.2f}".format(mean=df["err1"].mean(), std=np.sqrt(df["err1"].var())))
+        print(df["corruption"].to_list())
+        print(
+            "mean:{mean:0.2f}, std:{std:0.2f}".format(
+                mean=df["err1"].mean(), std=np.sqrt(df["err1"].var())
+            )
+        )
         print(" ")
 
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("-t", "--target_dir", type=str, help="target directory")
     parser.add_argument("--drop_gauss", action="store_true", default=False)
     opt = parser.parse_args()
