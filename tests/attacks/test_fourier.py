d5cec5fa505c4e16c1992feeb8d9b5c5fbb25ffb
gatheluck <gatheluck+tech@gmail.com>
2020-12-03 09:26:43 +0900

    add test code for gradient

--- tests/attacks/test_fourier.py
+++ tests/attacks/test_fourier.py
@@ -1,13 +1,10 @@
 import os
 import sys
-import math
 
 import torch
-import torchvision
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-from pytorch_fourier_analysis.attacks import FourierAttack, logits_to_index
-from pytorch_fourier_analysis import shared
+from pytorch_fourier_analysis.attacks import logits_to_index
 
 
 class TestLogitsToIndex:
@@ -17,26 +14,39 @@ class TestLogitsToIndex:
         noise = 0.01
         index_h = torch.randint(0, s, (batch_size,))
         index_w = torch.randint(0, s, (batch_size,))
-        index_h_onehot = torch.nn.functional.one_hot(index_h, num_classes=s).float()  # (B,S)
-        index_w_onehot = torch.nn.functional.one_hot(index_w, num_classes=s).float()  # (B,S)
+        index_h_onehot = torch.nn.functional.one_hot(
+            index_h, num_classes=s
+        ).float()  # (B,H)
+        index_w_onehot = torch.nn.functional.one_hot(
+            index_w, num_classes=s
+        ).float()  # (B,W)
 
-        index_ans = torch.matmul(index_h_onehot.unsqueeze(-1), index_w_onehot.unsqueeze(-2))
-        assert logits_to_index(index_h_onehot + noise, index_w_onehot + noise, scale_logits=10.).equal(index_ans)
+        index_ans = torch.matmul(
+            index_h_onehot.unsqueeze(-1), index_w_onehot.unsqueeze(-2)
+        )
+        assert logits_to_index(
+            index_h_onehot + noise, index_w_onehot + noise, scale_logits=10.0
+        ).equal(index_ans)
 
-    # def test_grad(self):
-    #     b = 8
-    #     s = 4
+    def test_grad(self):
+        batch_size = 8
+        s = 4
+        noise = 0.01
+        index_h = torch.randint(0, s, (batch_size,))
+        index_w = torch.randint(0, s, (batch_size,))
+        logits_h = (
+            torch.nn.functional.one_hot(index_h, num_classes=s).float() + noise
+        )  # (B,H)
+        logits_w = (
+            torch.nn.functional.one_hot(index_w, num_classes=s).float() + noise
+        )  # (B,W)
+        logits_h.requires_grad_()
+        logits_w.requires_grad_()
 
-    #     index_h = torch.randint(0, s, (b,))
-    #     index_w = torch.randint(0, s, (b,))
-    #     logits_h = torch.nn.functional.one_hot(index_h, num_classes=s)  # (B,S)
-    #     logits_w = torch.nn.functional.one_hot(index_w, num_classes=s)  # (B,S)
+        index = logits_to_index(logits_h, logits_w, scale_logits=10.0)
 
-    #     logits = torch.cat([logits_h.unsqueeze(1), logits_w.unsqueeze(1)], dim=1).float() + 0.01  # (B,2,S)
-    #     logits = logits.requires_grad_()
+        pseudo_loss = index.sum()
+        pseudo_loss.backward()
 
-    #     index = logits_to_index(logits * 5, tau=1)  # (B,2)
-    #     pseudo_loss = index.sum()
-    #     pseudo_loss.backward()
-
-    #     assert logits.grad is not None
+        assert logits_h.grad is not None
+        assert logits_w.grad is not None
